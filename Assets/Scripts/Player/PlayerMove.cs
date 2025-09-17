8ace39bab6a5d79ff2a4af0e2a9f979370372851
anton <anton3103@outlook.com>
2025-09-17 17:10:04 +0100

    Player start

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace NeverMindEver.Player {
+    public class PlayerMove {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
