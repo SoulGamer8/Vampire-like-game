88dca3f3996fda4363b249b23dc7dfbec0d9ce2e
anton <anton3103@outlook.com>
2025-09-18 13:38:22 +0100

    PlayerMove

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -1,17 +1,29 @@
 using UnityEngine;
 
 namespace NeverMindEver.Player {
-    public class PlayerMove {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+    public class PlayerMove:MonoBehaviour {
+        [SerializeField] private float speed = 5f; // одиниці в секунду
+        private Rigidbody2D _rb;
+        private Vector2 _moveInput;
+
+        void Awake()
         {
-        
+            _rb = GetComponent<Rigidbody2D>();
+            // Щоб гравець не крутился при фізичних зіткненнях:
+            _rb.freezeRotation = true;
         }
 
-        // Update is called once per frame
         void Update()
         {
-        
+            // Отримуємо ввід (WASD / стрілки) стандартними осями
+            float x = Input.GetAxisRaw("Horizontal"); // -1,0,1
+            float y = Input.GetAxisRaw("Vertical");
+            _moveInput = new Vector2(x, y).normalized; // нормалізуємо, щоб не бігати швидше по діагоналі
+        }
+
+        void FixedUpdate()
+        {
+            _rb.linearVelocity = _moveInput * speed;
         }
     }
 }
